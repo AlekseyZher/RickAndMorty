89d72d2ec38c938d493c4c9024f690639c645562
Алексей Жеребцов <aleksejzerebcov@MacBook-Pro-Aleksej.local>
2026-02-13 09:40:37 +0600

    фикс reamde.md, импортов

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 export { ErrorPage } from './ErrorPage/ErrorPage';
 export { NotFoundPage } from './NotFoundPage/NotFoundPage';
+export { Character } from './Character/Character';
+export { CharactersList } from './CharactersList/CharactersList'
